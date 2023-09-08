7e07a80e0d0fec7847004b15cc80a2e3bf05dec5
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-08 10:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let n=str.length;
+	for(let i=0;i<n-2;i+=2){
+		let char=str.charAt(i);
+		let char2=str.charAt(i+1);
+		if(char!=char2){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));  
